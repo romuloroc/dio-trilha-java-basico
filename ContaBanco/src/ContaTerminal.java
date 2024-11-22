110f1d0973d46b8172c5acc818dbb281bea45811
romuloroc <romulo.cardoso@ipremios.com>
2024-11-22 19:34:00 -0300

    Added created account

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -2,14 +2,11 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        // conhecer e inportar a classe Scanner
         Scanner scanner = new Scanner(System.in);
 
-        // Exibir mensagem para o nosso usuário
         System.out.println("Bem vindo ao Banco da DIO");
         System.out.println("Vamos criar uma conta para você");
 
-        // Obter pelo scanner os valores digitados no terminal
         System.out.println("Digite o número da agência: ");
         String agencia = scanner.nextLine();
 
@@ -19,13 +16,11 @@ public class ContaTerminal {
         System.out.println("Digite o seu nome: ");
         String nomeCliente = scanner.nextLine();
 
+        System.out.println("Digite o saldo inicial: ");
+        double saldo = scanner.nextDouble();
         
-        double saldo = 237.48;
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
         
-        // Exibir a mensagem conta criada
-        System.out.println("Olá" + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já esta disponível para saque.");
-        
-        // Close the scanner
         scanner.close();
     }
 }
